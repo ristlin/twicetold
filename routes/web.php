dbe4f58a6423cb1ec289a5411e57c66000839de4
Yuse Lajiminmuhip <yuse@tunnyl.com>
2021-09-13 17:27:06 +0800

    structuring welcome.vue

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
+        //'canLogin' => Route::has('login'),
+        //'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
